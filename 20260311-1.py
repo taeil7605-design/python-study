98008b29ff385c7b3c71c0ac94e9edf53caaeb73
taeil7605-design <taeil7605@gmail.com>
2026-03-10 16:58:57 +0000

    revie20260311_2

--- 20260311-1.py
+++ 20260311-1.py
@@ -1,5 +1,18 @@
-#for문 복습
-#n=int(input())
+# #for문 복습
+# #n=int(input())
 
-#for i in range(1,n+1):
-#    print(i)\\
+# #for i in range(1,n+1):
+# #    print(i)\\
+
+# 리스트 복습
+# n = int(input())
+# k=0
+# numbers=list(map(int, input().split()))
+
+# for i in numbers:
+#     k += num
+
+# print(k)
+
+
+
